3d9242dcbc651552dccda9911ed6c663d550092c
Armitage <listerine1989@gmail.com>
2017-08-22 23:33:08 -0500

    latest

--- app/scripts/services/constants.js
+++ app/scripts/services/constants.js
@@ -9,7 +9,7 @@
  */
 angular.module('shoplyApp')
   .constant('constants', {
-  	base_url : "http://ec2-52-15-89-223.us-east-2.compute.amazonaws.com:2020/api/",
+  	base_url : "//ec2-52-15-89-223.us-east-2.compute.amazonaws.com:2020/api/",
     socket : "http://ec2-52-15-89-223.us-east-2.compute.amazonaws.com:2020",                          
   	login_state_sucess : 'dashboard',
     uploadURL : "http://www.daimont.com:8080/api/uploads",
